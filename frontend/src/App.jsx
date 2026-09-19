997157dc185b2412881c7a7289432bd22b6fb750
kaviarasu1611 <kaviarasu16112000@gmail.com>
2026-09-20 01:08:52 +0530

    Fixed all

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_URL = import.meta.env.VITE_API_URL || "https://bulk-mail-backend-nnk2.onrender.com";
 
 function App() {
   const [subject, setSubject] = useState("");
